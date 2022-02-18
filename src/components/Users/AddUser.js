40c2c2638bcbd4e0183ed84011eeee1664d34fe6
wook <intuitively-@naver.com>
2022-02-18 13:20:58 +0900

    useRef()

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,72 @@
+import React, { useState, useRef } from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import classes from './AddUser.module.css';
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = nameInputRef.current.value;
+    event.preventDefault();
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
+      return;
+    }
+    props.onAddUser(enteredName, enteredUserAge);
+    enteredName.current.value = ''; //직접적으로 DOM을 조정하는 것인데 이렇게 잘 안써 진짜로
+    enteredUserAge.current.value = '';
+  };
+
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            ref={nameInputRef}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            ref={ageInputRef}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddUser;
